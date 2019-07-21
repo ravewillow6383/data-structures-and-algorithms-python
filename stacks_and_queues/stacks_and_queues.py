53a030f0f2409f99f4f4cda3918d4e9a958c54ea
Raven Robertson <ravewillow6383@gmail.com>
2019-07-20 18:07:10 -0700

    working on push test for stack

---
+++ stacks_and_queues/stacks_and_queues.py
@@ -0,0 +1,19 @@
+class Stack:
+    # Iterable is an object, which one can iterate over. It generates an Iterator when passed to iter() method
+  def __init__(self, iter=[]):
+    self.top = None
+    try:
+        for item in iter:
+            self.push(item)
+    except TypeError:
+        print('Oh no! Please insert an iterable.')
+
+  def push(self, value):
+    self.top = Node(value, self.top)
+    return self.top
+
+  
+class Node:
+  def __init__(self, value, next=None):
+      self.value = value
+      self.next = next   
