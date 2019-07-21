f62f4888664ac20944751ce2991a86060304d591
Raven Robertson <ravewillow6383@gmail.com>
2019-07-20 20:01:39 -0700

    stacks and queues passing all tests

--- stacks_and_queues/stacks_and_queues.py
+++ stacks_and_queues/stacks_and_queues.py
@@ -41,19 +41,40 @@ class Queue:
     
   def enqueue(self, value):
     
-      new_node = Node(value)
+    new_node = Node(value)
 
-      if not self.front:
-          self.front = new_node
-          self.back = new_node
-      else:
-        current = self.front
-        while current.next:
-          current = current.next
-        
-        current.next = new_node
-        self.back = current.next
-    
+    if not self.front:
+      self.front = new_node
+      self.back = new_node
+    else:
+      current = self.front
+      while current.next:
+        current = current.next
+      
+      current.next = new_node
+      self.back = current.next
+
+  def dequeue(self):
+
+    if not self.front:
+      return None
+    if self.front:
+      current = self.front
+      self.front = self.front.next
+      current.next = None
+      return current.value
+    else:
+      if self.back is not None:
+        self.back = None
+      raise ValueError('Empty Queue')
+  
+  def peek_queue(self):
+
+    if self.front:
+      return self.front.value
+    else:
+      raise ValueError('You have reached an empty queue.')
+ 
 class Node:
   def __init__(self, value, next=None):
       self.value = value
