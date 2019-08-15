2d2f1fee99e25a40a47983cec8af617ee174fe46
Raven Robertson <ravewillow6383@gmail.com>
2019-08-14 21:41:45 -0700

    passing tests

--- challenges/list_sorts/quick_sort/test_quick_sort.py
+++ challenges/list_sorts/quick_sort/test_quick_sort.py
@@ -7,6 +7,28 @@ def test_exists():
 def sort_single():
     assert quick_sort([4]) == [4]
 
-def test_sort_double():
-    assert quick_sort([2, 0]) == [0, 2]
+def test_randomly_unsorted_list():
+    """An unsorted list returns sorted"""
+    lst = [5, 2, 8, 1, 15]
+    
+    expected = [1, 2, 5, 8, 15]
+    actual = quick_sort(lst)
+    assert actual == expected
+
+
+def test_sorted_list():
+    """A sorted list will return the same"""
+    lst = [1, 2, 3, 4, 5]
+
+    expected = [1, 2, 3, 4, 5]
+    actual = quick_sort(lst)
+    assert actual == expected
+
+def test_empty_list():
+    """Edgecase, empty list"""
+    lst = []
+
+    expected = []
+    actual = quick_sort(lst)
+    assert actual == expected
 
