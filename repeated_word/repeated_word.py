9c70ea0eec5bd7dc8e7038a121f2d8cb2225f659
Raven Robertson <ravewillow6383@gmail.com>
2019-08-19 17:28:54 -0700

    added docstrings

--- repeated_word/repeated_word.py
+++ repeated_word/repeated_word.py
@@ -1,9 +1,13 @@
 from collections import Counter
+"""Impoting counter to create a hashTable out of my string"""
 
 def repeated_word(str):
+    """putting string into lower case and splitting up each word"""
     words = str.lower().split(' ')
+    """creating an iterable hashable dictionary"""
     dict = Counter(words)
 
+    """Looping through our words to find our first entry in our counter that's greater than 1"""
     for key in words:
         if dict[key] > 1:
             return key
