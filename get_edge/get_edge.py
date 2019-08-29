b4bac9f5f0f055b5b614d5817b780fbdd0d14c89
Raven Robertson <ravewillow6383@gmail.com>
2019-08-28 19:36:00 -0700

    I can't figure out why I can't get neighbors in this method. I don't know.

--- get_edge/get_edge.py
+++ get_edge/get_edge.py
@@ -1,23 +1,24 @@
-from queues import Queue
+from collections import deque
 from graph import Graph, Vertex, Edge
 
 
 def direct_flight(g, lst):
     g = Graph()
     cost = 0
-    q = Queue()
+    q = deque()
     
     for city in lst:
-        q.enqueue(city)
+        q.appendleft(city)
     
     while q:
-        current = q.dequeue()
+        current = q.pop()
         neighs = g.get_neighbors(current)
         n = len(neighs) - 1
 
         for i in range(0, n):
             if neighs[i].vertex.value == q.front.value:
                 cost += neighs[i].weight
-            else:
-                return f'{False}, $0'
+            
+        return f'{False}, $0'
+
     return f'{True}, ${cost}'
--- graphs/test_graphs.py
+++ graphs/test_graphs.py
@@ -102,3 +102,14 @@ def test_breadth_first():
     g.breadth_first(dogs, visit)
 
     assert visited == ['dogs', 'cats', 'ferrets']
+
+
+def test_depth_first():
+    g = Graph()
+    dogs = g.add_vertex('dogs')
+    cats = g.add_vertex('cats')
+    ferrets = g.add_vertex('ferrets')
+    g.add_edge(dogs, cats, 15)
+    g.add_edge(dogs, ferrets, 12)
+
+    assert g.depth_first(dogs) == ['dogs', 'cats', 'ferrets']
